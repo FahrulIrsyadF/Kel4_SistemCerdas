d037e16153608f7b3427168a4b795ad26121cd1a
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2021-05-28 23:02:13 +0700

    template n framework

---
+++ app/Controllers/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Index extends BaseController
+{
+	public function index()
+	{
+		$data['title'] = 'ini title';
+		echo view('dashboard', $data); 
+	}
+}
