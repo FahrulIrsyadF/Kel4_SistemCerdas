4569bf41137e2031bbd7ab976c2921fb88a8e727
mustikakhoiri <mustikakhoiri15@gmail.com>
2021-06-28 04:25:12 +0700

    printpdf

--- app/Views/v_laporan.php
+++ app/Views/v_laporan.php
@@ -18,16 +18,18 @@
                     <a href="#">Laporan Hasil</a>
                 </li>
             </ul>
+            <a class="btn btn-primary btn-round ml-auto align-items-right" href="<?= base_url('laporan/printPDF') ?>" target="_BLANK">
+                <i class="fa fa-print"> Cetak</i>
+            </a>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
                         <div class="d-flex align-items-center">
-                            <h4 class="card-title">Jumlah User Testing Berdasarkan Umur</h4>
-                            <a class="btn btn-primary btn-round ml-auto" href="<?= base_url('laporan/printPDF') ?>">
-                                <i class="fa fa-print"> Cetak</i>
-                            </a>
+                            <b>
+                                <h4 class="card-title">Jumlah User Testing Berdasarkan Umur</h4>
+                            </b>
                         </div>
                     </div>
                     <div class="card-body">
@@ -46,10 +48,50 @@
                                     foreach ($umur->getResultArray() as $um) { ?>
                                         <tr>
                                             <td><?= $nomor++; ?></td>
-                                            <td><?= $um['age_test']; ?>
+                                            <td><?= $um['age_test']; ?></td>
+                                            <td><?= $um['jumlah']; ?></td>
+                                            <td>
+                                                <div class="form-button-action">
+                                                    <button type="button" data-toggle="modal" data-target="#DetailModal" title="" data-id="" class="btn btn-link btn-primary btn-lg" data-original-title="Detail">
+                                                        <i class="fa fa-info-circle"></i>
+                                                    </button>
+                                                </div>
                                             </td>
-                                            <td><?= $um['jumlah']; ?>
-                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <?= $pager->links('bootstrap', 'custom_pagination') ?>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <b>
+                                <h4 class="card-title">Klasifikasi Berdasarkan Jumlah User Testing</h4>
+                            </b>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="add-row" class="display table table-hover">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Klasifikasi</th>
+                                    <th>Jumlah</th>
+                                    <th>Action</th>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $nomor = 1;
+                                    $klasifikasi = $db->query("SELECT class.name_class, COUNT(*) AS jumlah FROM test, class
+                                        WHERE test.id_class = class.id_class GROUP BY class.id_class");
+                                    foreach ($klasifikasi->getResultArray() as $kls) { ?>
+                                        <tr>
+                                            <td><?= $nomor++; ?></td>
+                                            <td><?= $kls['name_class']; ?></td>
+                                            <td><?= $kls['jumlah']; ?></td>
                                             <td>
                                                 <div class="form-button-action">
                                                     <button type="button" data-toggle="modal" data-target="#DetailModal" title="" data-id="" class="btn btn-link btn-primary btn-lg" data-original-title="Detail">
--- app/Views/v_printLaporan.php
+++ app/Views/v_printLaporan.php
@@ -7,8 +7,15 @@
 </head>
 
 <body>
+    <?php
+    date_default_timezone_set('Asia/Jakarta');
+    $tanggal = mktime(date("m"), date("d"), date("Y"));
+    echo "Tanggal : <b>" . date("d-M-Y", $tanggal) . "</b> ";
+    $jam = date("H:i:s");
+    echo "| Pukul : <b>" . $jam . " " . "</b>";
+    $a = date("H"); ?>
     <h3>
-        <center>Jumlah User Testing Berdasarkan Umur</center> <br>
+        <center>Jumlah User Testing Berdasarkan Umur</center>
     </h3>
     <table border="1" cellspacing="0" cellpadding="5" width="100%">
         <thead>
@@ -33,6 +40,38 @@
                     </td>
                     <td>
                         <?= $um['jumlah']; ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <br><br>
+    <h3>
+        <center>Klasifikasi Berdasarkan Jumlah User Testing</center>
+    </h3>
+    <table border="1" cellspacing="0" cellpadding="5" width="100%">
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Klasifikasi</th>
+                <th>Jumlah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $nomor = 1;
+            $klasifikasi = $db->query("SELECT class.name_class, COUNT(*) AS jumlah FROM test, class
+                                        WHERE test.id_class = class.id_class GROUP BY class.id_class");
+            foreach ($klasifikasi->getResultArray() as $kls) { ?>
+                <tr>
+                    <td>
+                        <center><?= $nomor++; ?></center>
+                    </td>
+                    <td>
+                        <?= $kls['name_class']; ?>
+                    </td>
+                    <td>
+                        <?= $kls['jumlah']; ?>
                     </td>
                 </tr>
             <?php } ?>
